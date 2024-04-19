dc14edb66781470c35a9555262356ca4bf159b16
harmanpreet09 <122574728+harmanpreet09@users.noreply.github.com>
2024-04-18 21:01:10 -0700

    Update the class name of accordian

--- accordian.js
+++ accordian.js
@@ -1,15 +1,15 @@
-document.querySelectorAll('.planting-faq__question').forEach(question => {
-    question.addEventListener('click', () => {
-      const answer = question.nextElementSibling;
-      const item = question.parentElement;
-  
-      item.classList.toggle('active');
-      question.setAttribute('aria-expanded', item.classList.contains('active'));
-      
-      if (item.classList.contains('active')) {
-        answer.style.display = 'block';
-      } else {
-        answer.style.display = 'none';
-      }
-    });
+document.querySelectorAll('.accordian__heading').forEach(question => {
+  question.addEventListener('click', () => {
+    const answer = question.nextElementSibling;
+    const item = question.parentElement;
+
+    item.classList.toggle('active');
+    question.setAttribute('aria-expanded', item.classList.contains('active'));
+    
+    if (item.classList.contains('active')) {
+      answer.style.display = 'block';
+    } else {
+      answer.style.display = 'none';
+    }
   });
+});
